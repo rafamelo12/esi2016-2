3a3ffaa1f539715e073faadad47b64b1954c8019
Rafael Oliveira <rafamelo.oliveira@gmail.com>
2016-09-26 18:35:13 -0300

    Pushing minor changes before checking out to another branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define() do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
